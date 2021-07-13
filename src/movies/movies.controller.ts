154c7437f46ee578fc9b423a7b76b8edd1ecb746
김현진 <khjzzm@gmail.com>
2021-07-13 14:48:35 +0900

    add movies

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Param,
+  Body,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for movies ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() data) {
+    return this.moviesService.create(data);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
